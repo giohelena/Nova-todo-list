2b62499c7291323e3c2fd82f2ade7e65f6d56b0b
Giovanna Helena <giovannahlopes98@gmail.com>
2022-09-18 13:16:30 -0300

    base do projeto todo-list

---
+++ config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/todolist', {useNewUrlParset: true, useUnifiedTopoly: true})
+.then(()=> console.log('Conectado ao MongoDB'))
+.catch((err) => console.error(err))
